94c84b66433679eaacd7daa860eeab8e968212ba
monpopo <bak.the.bacon@gmail.com>
2025-10-19 03:28:28 +0700

    Create src_styles_theme.js

---
+++ src_styles_theme.js
@@ -0,0 +1,15 @@
+// Pastel color palette inspired by your design images
+const theme = {
+  colors: {
+    blue: "#81C7E3",
+    lavender: "#B7A6E8",
+    pink: "#E8A6CF",
+    purple: "#A6ABE8",
+    lightBlue: "#CCE6F6",
+    lightLavender: "#E3D6F7",
+    lightPink: "#F7D6EB",
+    white: "#FFFFFF"
+  }
+};
+
+export default theme;
